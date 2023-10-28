17d97c12a5986d05652060c5a8cc66f9b348bb8a
Nobuhiro Ito <iseebi@iseteki.net>
2023-10-28 21:46:47 +0900

    remove pages

--- src/renderer/src/components/pages/NotFoundPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NotFoundPage: React.FC = () => <div>NotFoundPage</div>;
-
-export default NotFoundPage;
--- src/renderer/src/components/pages/TopPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TopPage: React.FC = () => <div>TopPage</div>;
-
-export default TopPage;
