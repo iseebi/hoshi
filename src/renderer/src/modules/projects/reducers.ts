43ca2f2cad77d4dfce1b3a4ced0f846fa0eac0ea
Nobuhiro Ito <iseebi@iseteki.net>
2023-11-04 01:05:32 +0900

    handle project open cancel

--- src/renderer/src/modules/projects/reducers.ts
+++ src/renderer/src/modules/projects/reducers.ts
@@ -2,6 +2,7 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { EmptyProjectsState } from './types';
 import { registerProgressAction } from '../actionCreatorsHelpers';
 import { fetchCurrentProjectProgressAction, openProjectProgressAction, switchPackageAction } from './actions';
+import { LoadingState } from '../models';
 
 const reducer = reducerWithInitialState({ ...EmptyProjectsState });
 
@@ -12,8 +13,11 @@ registerProgressAction(reducer, fetchCurrentProjectProgressAction, (state, { val
 
 registerProgressAction(reducer, openProjectProgressAction, (state, { value }) => ({
   ...state,
-  project: value,
-  activePackage: null,
+  project: {
+    loadingState: value.loadingState,
+    value: value.loadingState === LoadingState.Initial ? value.value ?? state.project?.value : state.project?.value,
+  },
+  activePackage: value.loadingState === LoadingState.Initial && value.value ? null : state.activePackage,
 }));
 
 reducer.case(switchPackageAction, (state, { packageId }) => ({ ...state, activePackage: packageId }));
--- src/renderer/src/modules/projects/selectors.ts
+++ src/renderer/src/modules/projects/selectors.ts
@@ -1,10 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { ProjectsState, SmalledProject } from './types';
-import { LoadingState } from '../models';
 
 type State = { projects: ProjectsState };
 
-export const selectCurrentProject = (state: State): SmalledProject | undefined =>
-  state.projects.project.loadingState === LoadingState.Initial ? state.projects.project.value : undefined;
+export const selectCurrentProject = (state: State): SmalledProject | undefined => state.projects.project.value;
 
 export const selectHasCurrentProject = createSelector(selectCurrentProject, (project) => Boolean(project));
