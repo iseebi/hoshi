08b6237ab89682f564f70d7c081be0f10261e0e6
Nobuhiro Ito <iseebi@iseteki.net>
2024-03-31 12:14:16 +0900

    add JSON format

--- src/engine/src/converters/index.ts
+++ src/engine/src/converters/index.ts
@@ -2,8 +2,10 @@ import { Converter } from './type';
 import AppleStringsConverter from './appleStrings';
 import AndroidXmlConverter from './androidXml';
 import I18NextConverter from './i18next';
+import JsonConverter from './json';
 
 export type { Converter };
 export { AppleStringsConverter };
 export { AndroidXmlConverter };
 export { I18NextConverter };
+export { JsonConverter };
