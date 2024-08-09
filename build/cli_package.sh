b7c6eec9a3017cfbaa2b47b69414f42218a41e0a
Nobuhiro Ito <iseebi@iseteki.net>
2024-08-09 23:14:52 +0900

    add cli notarization

--- build/cli_package.sh
+++ build/cli_package.sh
@@ -6,19 +6,38 @@ function buildPackage() {
   arch=$2
   ext=$3
 
-  echo "test $pf $arch $ext"
-
   tag="$pf-$arch"
 
-  mkdir -p "out/cli/$tag"
-  cp "out/cli/index-$tag$ext" "out/cli/$tag/hoshi-cli$ext"
   pushd "out/cli/$tag"
   zip "../${npm_package_name}-cli-${npm_package_version}-$tag.zip" "hoshi-cli$ext"
   popd
 }
 
+function buildMacDmg() {
+  pf="mac"
+  arch="arm64"
+  tag="$pf-$arch"
+
+  hdiutil create -volname "YourVolumeName" -srcfolder "out/cli/$tag" -ov -format UDZO out/cli/${npm_package_name}-cli-${npm_package_version}-$tag.dmg
+}
+
+function notarizeMacPackages() {
+  pf="mac"
+  arch="arm64"
+  tag="$pf-$arch"
+
+  base="out/cli/$tag/${npm_package_name}-cli-${npm_package_version}-$tag."
+
+  xcrun notarytool submit "$base.zip" --key "$KEY_PATH" --key-id "$KEY_ID" --key-issuer "$ISSUER_ID"
+  xcrun notarytool submit --wait "$base.dmg"
+  xcrun stapler staple "$base.dmg"
+}
+
 buildPackage "win" "x64" ".exe"
 buildPackage "mac" "arm64" ""
 buildPackage "linux" "x64" ""
+
+buildMacDmg
+notarizeMacPackages
 
 cp out/cli/*.zip dist/
