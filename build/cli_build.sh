4b45af7dc7ae40d7b27df3741c5c4640febfd592
Nobuhiro Ito <iseebi@iseteki.net>
2024-11-24 16:03:24 +0900

    update signing cli for hardened runtime

--- build/cli_build.sh
+++ build/cli_build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 webpack --config ./webpack-cli.config.js
 pkg -o out/cli/win-x64/hoshi-cli.exe --targets node18-win-x64 out/cli/src/index.js
-pkg -o out/cli/mac-arm64/hoshi-cli --targets node18-mac-arm64 out/cli/src/index.js --no-signature
+pkg -o out/cli/mac-arm64/hoshi-cli --targets node18-mac-arm64 out/cli/src/index.js
 pkg -o out/cli/linux-x64/hoshi-cli --targets node18-linux-x64 out/cli/src/index.js
 
 pf=$(uname)
 if [ $pf = "Darwin" ]; then
   cert_name=$(security find-certificate -c "Developer ID Application" | grep "alis" | awk -F'"' '{print $4}')
-  codesign -f -v -s "$cert_name" -o runtime --no-strict --deep out/cli/mac-arm64/hoshi-cli
+  codesign -f -v --sign "$cert_name" -o runtime --deep --entitlements hoshi-cli.entitlements out/cli/mac-arm64/hoshi-cli
 fi
