30a0c27edb7245fa38f6ebe094a4c59b07a5cf87
Nobuhiro Ito <iseebi@iseteki.net>
2024-12-21 22:12:40 +0900

    wait for staple

--- build/notarization.sh
+++ build/notarization.sh
@@ -6,6 +6,7 @@ xcrun notarytool submit --wait "$target" --key "$KEY_PATH" --key-id "$KEY_ID" --
 
 # dmgの場合はstaple
 if [[ $target == *.dmg ]]; then
+  sleep 5
   xcrun stapler staple "$target"
 fi
 
